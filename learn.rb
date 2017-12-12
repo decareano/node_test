c9c995596ddcf6ceafafc9b5614ddf9dcbc528be
decareano <marcelo.gobelli@gmail.com>
2017-12-12 07:21:25 -0800

    creating apache group

--- learn.rb
+++ learn.rb
@@ -1,3 +1,25 @@
 package "apache" do
+	package_name "apache2"
+end
+
+service 'apache2' do
+	action [:enable, :start]
 
 end
+
+# file '/var/www/index.html' do
+# 	action :delete
+group 'name' do
+	group_name 'apache'
+	action :create
+end
+
+
+file '/var/www/html/index.html' do
+	content 'Hello my dear internet'
+	mode '0755'
+	owner 'root'
+	group 'apache'
+end
+
+
